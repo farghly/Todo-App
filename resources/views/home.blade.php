0aa1fd8e48543acc875d1adaedf13de6b5de5156
farghly <eng_farghlyahmed@yahoo.com>
2022-05-30 14:48:35 +0200

    add views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="text-center">
+        <h1>Welcome to Laravel ToDo App</h1>
+        <hr/>
+
+        @include('partials.flash_notification')
+
+        <h4><a href="https://stackoverflow.com/users/5661396/eng-farghly" target="_blank"><h3>Farghly ahmed</h3></a></h4>
+    </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<hr/>
+
+<div class="container">
+    &copy; {{ date('Y') }}, <a href="https://stackoverflow.com/users/5661396/eng-farghly" target="_blank">Farghly ahmed</a>
+    <br/>
+</div>
